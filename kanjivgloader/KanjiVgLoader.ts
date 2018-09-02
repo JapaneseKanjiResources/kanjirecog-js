661d2446c380c56f81c7b9f226a0f90385578ed6
cobysy <cobysy@users.noreply.github.com>
2018-09-02 22:08:25 +0200

    mock InputStroke

--- kanjivgloader/KanjiVgLoader.ts
+++ kanjivgloader/KanjiVgLoader.ts
@@ -2,9 +2,14 @@ import sax from "sax";
 import fs from "fs";
 
 const strict = true;
-const parser = sax.parser(strict, {});
 
 const saxStream = sax.createStream(strict, {});
+
+saxStream.on("opentag", (node) => {
+    // same object as above
+    console.log(node);
+});
+
 fs.createReadStream("../data/kanjivg-20160426.xml")
     .pipe(saxStream)
     .pipe(fs.createWriteStream("../data/strokes.xml"));
--- src/InputStroke.ts
+++ src/InputStroke.ts
@@ -1,2 +1,17 @@
+import { Stroke } from "./Stroke";
+
 export class InputStroke {
+    /**
+	 * Normalises an array of strokes by converting their co-ordinates to range
+	 * from 0 to 1 in each direction. If the stroke bounding rectangle
+	 * has width or height 0, this will be handled so that it is at 0.5 in
+	 * the relevant position.
+	 * <p>
+	 * This works by constructing new stroke objects; strokes are final.
+	 * @param strokes Stroke array to convert
+	 * @return Resulting converted array
+	 */
+    public static normalise(strokes: InputStroke[]): Stroke[] {
+        return [];
+    }
 }
