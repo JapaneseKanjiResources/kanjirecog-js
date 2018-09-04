a52379c23a4de92175485393f944577ebcf608a5
cobysy <cobysy@users.noreply.github.com>
2018-09-05 00:27:02 +0200

    add testSvgInputMatch test

--- tests/FuzzyComparer.test.ts
+++ tests/FuzzyComparer.test.ts
@@ -3,10 +3,40 @@ import { assert } from "chai";
 import { KanjiList } from "../src/KanjiList";
 import { MatchAlgorithm } from "../src/MatchAlgorithm";
 import { KanjiVgLoaderTest } from "./KanjiVgLoader.test";
+/*
+import fs from "fs";
+import { KanjiInfo } from "../src/KanjiInfo";
+import { InputStroke } from "../src/InputStroke";
+*/
 
 describe("FuzzyComparer.test", () => {
 
     const DEBUG = false;
+
+    /*
+    it("testSvgInputMatch", async () => {
+        const list = new KanjiList();
+        const readAll = await KanjiVgLoaderTest.getAll();
+        for (const kanjiInfo of readAll) {
+            list.add(kanjiInfo);
+        }
+        list.finish();
+
+        const svg = fs.readFileSync("./tests/yama-handwrit.svg", "utf8");
+        const re = /path d="(.*?)"/g;
+        let m;
+        do {
+            m = re.exec(svg);
+            if (m) {
+                console.log(m[1]);
+            }
+        } while (m);
+
+        const potentialKanji = new KanjiInfo("?");
+        potentialKanji.addStroke(InputStroke.fromSvgPath(""));
+
+    });
+    */
 
     it("testBigMatch", async () => {
         // This compares the first 5 20-stroke kanji characters against all the
